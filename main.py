9a4939eb2b32fbb72c4fa0319d566e0e8aa4fe1b
Michael Halim <michaelhalimsenatra@gmail.com>
2022-10-14 20:31:11 +0700

    Progress 14 Oct: - Added Insert and Update Feature for Releveant DB Columns - Added Distance Calculation - Vectorized Color, Material, Furniture Location, Description, and Title - Add Bag of Words method to categorical data

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import psycopg2
 import psycopg2.extras
 import os
 from slugify import slugify
+from ml_helper import *
 
+import numpy as np
+import time 
+import datetime
 # Import All Data from each module
 def get_all_data():
     try:
@@ -219,11 +223,15 @@ def transfer_data_to_database():
                                     dimension_unit, 
                                     "isProduct", 
                                     furniture_location)
-                                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+                                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                                    RETURNING id '''
 
                 all_records = get_all_data()
                                 
                 print('=========================================')
+                all_ids = []
+                all_material, all_title, all_title, all_description, all_weight, all_dimension_length = [], [], [], [], [], []
+                all_dimension_width, all_dimension_height, all_price, all_furniture_location, all_color = [], [], [], [], []
                 for record in all_records:
                     for data in record[0]:
                         weight =  float(data['weight']) if data['weight'] else 0
@@ -232,9 +240,20 @@ def transfer_data_to_database():
                         dimension_height =  float(data['dimension_height']) if data['dimension_height'] else 0
                         isProduct = True if data['isProduct'] == 1 else False
                         slug = slugify(data['name'])
-
                         if len(str(dimension_length)) > 5:
                             dimension_length =  float(str(dimension_length)[:3] + '.00')
+                        
+
+                        all_material.append(data['material'])
+                        all_color.append(data['color'])
+                        all_description.append(data['description'])
+                        all_title.append(data['name'])
+                        all_weight.append(weight)
+                        all_dimension_length.append(dimension_length)
+                        all_dimension_height.append(dimension_height)
+                        all_dimension_width.append(dimension_width)
+                        all_price.append(data['price'])
+                        all_furniture_location.append(','.join(data['furnitureLocation']))
 
                         tmp_data = (data['name'], 
                                     slug,
@@ -256,7 +275,158 @@ def transfer_data_to_database():
                                     isProduct,
                                     ','.join(data['furnitureLocation']))
                         cur.execute(insert_script, tmp_data)
+
+                        _id = cur.fetchone()
+                        all_ids.append(int(_id[0]))
                     print(record[1] + ' DATA INSERTED SUCCESSFULY')
+                
+                # Get Vectorized_X and Feature Names for Categorical Featuers
+                vectorized_X_color, color_feature_names = bag_of_words(all_color)
+                vectorized_X_material, material_feature_names = bag_of_words(all_material, delimiter='comma')
+                vectorized_X_furniture_location, furniture_location_feature_names = bag_of_words(all_furniture_location, delimiter='comma')
+                vectorized_X_description, description_feature_names = bag_of_words(all_description)
+                vectorized_X_title, title_feature_names = bag_of_words(all_title)
+
+                # Get Distances for Categorical Features
+                color_distances = find_all_distance(vectorized_X_color,is_normalized_data=True, is_standard_scaler=False)
+                material_distances = find_all_distance(vectorized_X_material,is_normalized_data=True, is_standard_scaler=False)
+                description_distances = find_all_distance(vectorized_X_description,is_normalized_data=True, is_standard_scaler=False)
+                title_distances = find_all_distance(vectorized_X_title,is_normalized_data=True, is_standard_scaler=False)
+                furniture_location_distances = find_all_distance(vectorized_X_furniture_location,is_normalized_data=True, is_standard_scaler=False)
+
+                # Reshape All Numerical Features
+                all_weight = np.asarray(all_weight).reshape(-1, 1)
+                all_price = np.asarray(all_price).reshape(-1, 1)
+
+                all_dimension = []
+
+                for length, width, height in zip(all_dimension_length, all_dimension_width, all_dimension_height):
+                    length = float(length) if length != '' else 0
+                    width = float(width) if width != '' else 0
+                    height = float(height) if height != '' else 0
+                    all_dimension.append([length , width, height ])
+
+
+                weight_distances = find_all_distance(all_weight,is_normalized_data=True, is_standard_scaler=False)
+                dimension_distances = find_all_distance(all_dimension,is_normalized_data=True, is_standard_scaler=False)
+                price_distances = find_all_distance(all_price,is_normalized_data=True, is_standard_scaler=False)
+
+
+                start_time = time.perf_counter()
+                insert_distance_script = '''
+                    INSERT INTO public.main_app_distance (
+                        product_id, 
+                        other_product_id, 
+                        color_distance, 
+                        name_distance, 
+                        description_distance, 
+                        material_distance, 
+                        weight_distance,  
+                        dimension_distance, 
+                        price_distance, 
+                        furniture_location_distance,
+                        total_distance,
+                        temp_distance
+                    )
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s );
+                '''
+                
+                for i in range(len(all_ids)):
+                    for j in range(i, len(all_ids)-1):
+                        if i % 400 == 0 and j % 400 == 0:
+                            print('====================================')
+                            print(f'{i}th => {j}th')
+                            print(all_ids[i])
+                            print(all_ids[j])
+                            print(color_distances[i][j])
+                            print(f'{start_time} has passed')
+                            print('====================================')
+                        total_distance = float(color_distances[i][j]) + float(title_distances[i][j]) + float(description_distances[i][j]) + float(material_distances[i][j]) \
+                                + float(weight_distances[i][j]) + float(dimension_distances[i][j]) + float(price_distances[i][j]) + float(furniture_location_distances[i][j])
+
+                        tmp_data = (
+                            all_ids[i] ,
+                            all_ids[j],
+                            color_distances[i][j],
+                            title_distances[i][j],
+                            description_distances[i][j],
+                            material_distances[i][j],
+                            weight_distances[i][j],
+                            dimension_distances[i][j],
+                            price_distances[i][j],
+                            furniture_location_distances[i][j],
+                            total_distance,
+                            99.0
+                        )
+
+                        cur.execute(insert_distance_script,tmp_data)
+
+                print('TOTAL INSERTING DISTANCE')
+                print('--- %s ---' % (datetime.timedelta(seconds = time.perf_counter() - start_time)))
+
+
+                start_time = time.perf_counter()
+
+                update_item_script = '''
+                UPDATE public.main_app_item
+                SET vect_color = %s, 
+                    vect_material = %s, 
+                    vect_description = %s, 
+                    vect_name = %s, 
+                    vect_furniture_location = %s,
+                    normalized_weight = %s, 
+                    normalized_dimension = %s,
+                    normalized_price = %s
+                WHERE id = %s;
+                '''
+                vectorized_X_color = vectorized_X_color.tolist()
+                vectorized_X_material = vectorized_X_material.tolist()
+                vectorized_X_description = vectorized_X_description.tolist()
+                vectorized_X_title = vectorized_X_title.tolist()
+                vectorized_X_furniture_location = vectorized_X_furniture_location.tolist()
+                
+                for vector_color, vector_material, vector_description, vector_name, vector_furniture_location, _id, weight, dimension, price in \
+                    zip( vectorized_X_color, vectorized_X_material, vectorized_X_description, vectorized_X_title, vectorized_X_furniture_location , all_ids, all_weight, all_dimension, all_price):
+                    
+                    tmp_data = (
+                        vector_color, 
+                        vector_material,
+                        vector_description,
+                        vector_name,
+                        vector_furniture_location,
+                        weight[0],
+                        dimension[0],
+                        price[0],
+                        _id
+                    )
+
+                    cur.execute(update_item_script,tmp_data)
+                
+                print('TOTAL UPDATING VECTOR')
+                print('--- %s ---' % (datetime.timedelta(seconds = time.perf_counter() - start_time)))
+
+
+                start_time = time.perf_counter()
+
+                insert_feature_script = '''
+                INSERT INTO public.main_app_feature(
+                    name_feature, color_feature, material_feature, description_feature, 
+                    furniture_location_feature )
+                    VALUES (%s, %s, %s, %s, %s);
+                '''
+                tmp_data = (
+                    title_feature_names,
+                    color_feature_names,
+                    material_feature_names,
+                    description_feature_names,
+                    furniture_location_feature_names
+                )
+                cur.execute(insert_feature_script,tmp_data)
+
+                print('TOTAL INSERTING FEATURE')
+                print('--- %s ---' % (datetime.timedelta(seconds = time.perf_counter() - start_time)))
+
+                print(color_feature_names)
                 print('=========================================')
                 
                 # for record in insert_values:
